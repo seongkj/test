d370f6c3c39e7ad7b0cf822ef51e7cdbaff52b38
seongkj <59130927+seongkj@users.noreply.github.com>
2022-02-08 21:38:06 +0900

    test

---
+++ app.js
@@ -0,0 +1,15 @@
+const a = 5;
+const b = 2;
+const verylongvariablename = 0; //변수이름에 띄어쓰기를 하고싶다면 _나 대문자를 쓴다
+const c = 'asdf';
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(c);
+let d;
+console.log(d); //undefined 출력
+const e = null;
+console.log(e);
+const f = true; //booleans
+
